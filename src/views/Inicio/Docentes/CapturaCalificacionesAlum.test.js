4e58bb4ce86c2c49e851486d2ab9246d916f4857
Eduardo Domínguez Librado <eduardodominguez539@gmail.com>
2024-10-26 18:50:50 -0600

    Pruebas unitarias Login Jest CI

---
+++ src/views/Inicio/Docentes/CapturaCalificacionesAlum.test.js
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom'; // Importa jest-dom
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import CapturaCalificacionesAlum from './CapturaCalificacionesAlum';
+import { BrowserRouter } from 'react-router-dom';
+import { message } from 'antd';
+
+
