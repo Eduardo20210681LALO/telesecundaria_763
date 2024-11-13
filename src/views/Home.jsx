070621f2dbc86b6db3d40e0421e5d9b09ebc1a88
Eduardo20210681LALO <20210681@uthh.edu.mx>
2024-11-13 01:44:11 -0600

    Version 11.11.17 Despliegue Integracion Continua

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import uno from '../images/uno.png';//se usa
 
+import uno from '../images/uno.png'; // se usa
+
+// Importaciones de imágenes
 import Imagen1 from '../images/Imagen1.jpg';
 import Imagen2 from '../images/Imagen2.jpg';
 import Imagen3 from '../images/Imagen3.jpeg';
@@ -18,11 +20,7 @@ import work1 from '../images/work1.jpg';
 import work2 from '../images/work2.jpg';
 import work3 from '../images/work3.jpg';
 
-
-
 import BreadCrumb from "./BreadCrumbView";
-
-
 
 function Home() {
   return (
@@ -38,7 +36,6 @@ function Home() {
               <span className="home__title-color">TeleSecundaria 763</span>
             </h1>
             <Link to="/login" className="button" style={{textDecoration: 'none'}}>Inicio de sesión</Link>
-
           </div>
 
           <div className="home__social">
@@ -46,6 +43,7 @@ function Home() {
             <a href="#" className="home__social-icon"><i className='bx bxl-facebook'></i></a>
             <a href="#" className="home__social-icon"><i className='bx bxl-instagram'></i></a>
           </div>
+
           <div className="home__img" style={{ clipPath: 'ellipse(50% 50% at 50% 50%)', backgroundColor: '#7d0430', marginTop: '-60px' }}>
             <img src={uno} alt="TeleSecundaria 763" />
           </div>
@@ -58,21 +56,22 @@ function Home() {
               <div className="col-lg-6">
                 <Slider autoplay={true} autoplaySpeed={3000}>
                   <div>
-                    <img src={Imagen1} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={Imagen1} alt="Imagen 1" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                   <div>
-                    <img src={Imagen2} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={Imagen2} alt="Imagen 2" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                   <div>
-                    <img src={Imagen3} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={Imagen3} alt="Imagen 3" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                 </Slider>
               </div>
 
               <div className="col-lg-6">
-                <p className="about__text" style={{ color: '#333', fontWeight: 'bold' }}>Somos una escuela que logra un trabajo colaborativo entre dirección, docentes, personal de apoyo, alumnos(as) y padres de familia, para obtener aprendizajes significativos y lograr los objetivos de la nueva escuela mexicana.</p>
+                <p className="about__text" style={{ color: '#333', fontWeight: 'bold' }}>
+                  Somos una escuela que logra un trabajo colaborativo entre dirección, docentes, personal de apoyo, alumnos(as) y padres de familia, para obtener aprendizajes significativos y lograr los objetivos de la nueva escuela mexicana.
+                </p>
               </div>
-
             </div>
           </div>
         </section>
@@ -84,18 +83,21 @@ function Home() {
               <div className="col-lg-6 order-lg-last">
                 <Slider autoplay={true} autoplaySpeed={3000}>
                   <div>
-                    <img src={Imagen4} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={Imagen4} alt="Imagen 4" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                   <div>
-                    <img src={Imagen5} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={Imagen5} alt="Imagen 5" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                   <div>
-                    <img src={Imagen6} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={Imagen6} alt="Imagen 6" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                 </Slider>
               </div>
+
               <div className="col-lg-6 order-lg-first">
-                <p className="about__text" style={{ color: '#333', fontWeight: 'bold' }}>Ser una institución reconocida por su desempeño académico y formativo, capaz de proveer a nuestros alumnos(as) conocimientos, habilidades, destrezas y aptitudes que les permita ser competentes y participar activamente en la sociedad cambiante a la que pertenecen.</p>
+                <p className="about__text" style={{ color: '#333', fontWeight: 'bold' }}>
+                  Ser una institución reconocida por su desempeño académico y formativo, capaz de proveer a nuestros alumnos(as) conocimientos, habilidades, destrezas y aptitudes que les permita ser competentes y participar activamente en la sociedad cambiante a la que pertenecen.
+                </p>
               </div>
             </div>
           </div>
@@ -108,18 +110,21 @@ function Home() {
               <div className="col-lg-6">
                 <Slider autoplay={true} autoplaySpeed={3000}>
                   <div>
-                    <img src={work1} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={work1} alt="Work 1" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                   <div>
-                    <img src={work2} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={work2} alt="Work 2" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                   <div>
-                    <img src={work3} alt="" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
+                    <img src={work3} alt="Work 3" className="img-fluid rounded mx-auto d-block" style={{ width: 'auto', height: '300px' }} />
                   </div>
                 </Slider>
               </div>
+
               <div className="col-lg-6">
-                <p className="about__text" style={{ color: '#333', fontWeight: 'bold' }}>Puntualidad, Responsabilidad, Compromiso, Respeto, Disciplina, Empatía, Actitud de Servicio, Liderazgo, Igualdad, Resiliencia.</p>
+                <p className="about__text" style={{ color: '#333', fontWeight: 'bold' }}>
+                  Puntualidad, Responsabilidad, Compromiso, Respeto, Disciplina, Empatía, Actitud de Servicio, Liderazgo, Igualdad, Resiliencia.
+                </p>
               </div>
             </div>
           </div>
@@ -127,9 +132,7 @@ function Home() {
         
       </main>
       <BreadCrumb/>
-
       <Footer/>
-     
     </div>
   )
 }
